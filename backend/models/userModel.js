084a127226d01a9df5518bbe0bd4fc770a0196b8
RohitShalgar4 <rohishalgar04@gmail.com>
2025-02-14 23:45:52 +0530

    added schema for user or student

---
+++ backend/models/userModel.js
@@ -0,0 +1,101 @@
+import mongoose from "mongoose";
+
+const userModel = new mongoose.Schema({
+    full_name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    Mobile_No: {
+        type: String,
+        required: true
+    },
+    Parent_No: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    registration_number: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    Department: {
+        type: String,
+        required: true
+    },
+    class: {
+        type: String,
+        required: true
+    },
+    abc_id: {
+        type: String,
+        required: true
+    },
+    previous_cgpa: {
+        type: [Number],
+        required: true
+    },
+    previous_percentages: {
+        type: [Number],
+        required: true
+    },
+    current_semester: {
+        type: Number,
+        required: true
+    },
+    class_rank: {
+        type: Number,
+        required: true
+    },
+    attendance: [{
+        month: {
+            type: String,
+            required: true
+        },
+        attendance: {
+            type: Number,
+            required: true
+        }
+    }],
+    semester_results: [{
+        semester: {
+            type: Number,
+            required: true
+        },
+        cgpa: {
+            type: Number,
+            required: true
+        },
+        percentage: {
+            type: Number,
+            required: true
+        }
+    }],
+    photo_url: {
+        type: String,
+        required: true
+    },
+    achievements: {
+        type: [String],
+        required: true
+    },
+    gender: {
+        type: String,
+        enum: ["Male", "Female", "Other"],
+        required: true
+    }
+}, { timestamps: true });
+
+export const User = mongoose.model("User", userModel);
