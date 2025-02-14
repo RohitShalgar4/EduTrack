796b7e632414871e95c7c8a8585b386f9bfa33e7
RohitShalgar4 <rohishalgar04@gmail.com>
2025-02-14 23:49:34 +0530

    added router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
+import Login from './pages/Login'
+import StudentDashboard from './pages/student/StudentDashboard';
+import MainLayout from './components/Layout';
+
+// For debugging purposes
+console.log('MainLayout:', MainLayout);
+console.log('Login:', Login);
+console.log('StudentDashboard:', StudentDashboard);
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      { 
+        path: "/", // or index: true
+        element: <StudentDashboard /> 
+      },
+      { 
+        path: "login", 
+        element: <Login />,
+      },
+      { 
+        path: "dashboard", 
+        element: <StudentDashboard /> 
+      },
+    ],
+  },
+]);
 
 function App() {
-
   return (
-    <>
-      <h1 className='bg-blue-400'>Let's Build EduTrack</h1>
-    </>
+    <RouterProvider router={appRouter} />
   )
 }
 
