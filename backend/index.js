ac07aa6265736caed7ff2881b3e6bb276c68a63d
RohitShalgar4 <rohishalgar04@gmail.com>
2025-02-14 23:46:58 +0530

    added routes for student

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
-import userRoute from "./routes/user.route.js";
+import userRoute from "./routes/userRoute.js";
+import studentRoute from './routes/studentRoute.js';
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -14,12 +15,26 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cookieParser());
+const allowedOrigins = ['http://localhost:5173'];
+
 app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-  })
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true); // Allow the request
+            } else {
+                callback(new Error('Not allowed by CORS')); // Block the request
+            }
+        },
+        methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+        credentials: true, // Allow cookies and auth headers
+    })
 );
+
+// Routes
+
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/student", studentRoute);
 
 //APIs
 
