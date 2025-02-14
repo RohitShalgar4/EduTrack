2bd8b46007dffc3173171d6e5171b79b46234203
RohitShalgar4 <rohishalgar04@gmail.com>
2025-02-14 23:54:15 +0530

    added store.js

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from './userSlice'
+import studentReducer from './studentSlice'
+
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+    student: studentReducer,
+  },
+})
+
+export default store;
