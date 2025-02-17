628f3f3ccf8a1d223a0ae673ad8b7d1169c6bbb5
RohitShalgar4 <rohishalgar04@gmail.com>
2025-02-18 01:09:56 +0530

    update controller of login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -93,9 +93,12 @@ export const login = async (req, res) => {
             _id: user._id,
             username: user.username,
             fullName: user.fullName,
-            profilePhoto: user.profilePhoto
+            profilePhoto: user.profilePhoto,
+            message: "Logged in successfully.",
+            success: true,
+            role: user.role || "student"
         });
-
+        
     } catch (error) {
         console.log(error);
     }
