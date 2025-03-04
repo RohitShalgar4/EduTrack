9d0f90a3c08181ab11e1b35f50db52e3ca052ff4
RohitShalgar4 <rohishalgar04@gmail.com>
2025-03-04 23:05:12 +0530

    added redux persister

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 import { Provider } from 'react-redux';
-import store from './redux/store.js';
+import { PersistGate } from 'redux-persist/integration/react';
+import store, { persistor } from './redux/store.js';
 
 export const BASE_URL = "http://localhost:8080"; 
 
 createRoot(document.getElementById('root')).render(
-  <Provider store={store}> 
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
