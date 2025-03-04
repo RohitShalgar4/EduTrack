149661b37644b061fc1cdc89b9dc9b82cba6cc40
RohitShalgar4 <rohishalgar04@gmail.com>
2025-03-04 23:04:11 +0530

    changes for storing user

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,12 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './userSlice'
-import studentReducer from './studentSlice'
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userReducer from './userSlice';
+import studentReducer from './studentSlice';
+
+const userPersistConfig = {
+  key: 'user',
+  storage,
+  whitelist: ['authUser'],
+};
+
+const studentPersistConfig = {
+  key: 'student',
+  storage,
+  whitelist: ['data'],
+};
+
+const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
+const persistedStudentReducer = persistReducer(studentPersistConfig, studentReducer);
 
 const store = configureStore({
   reducer: {
-    user: userReducer,
-    student: studentReducer,
+    user: persistedUserReducer,
+    student: persistedStudentReducer,
   },
-})
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE'],
+      },
+    }),
+});
 
+export const persistor = persistStore(store);
 export default store;
