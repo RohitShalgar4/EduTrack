3995ebdf6db6c8c07918b29fadc02adf9d8be193
RohitShalgar4 <rohishalgar04@gmail.com>
2025-03-16 16:55:36 +0530

    added route for the updatepass

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,5 +1,6 @@
 import express from "express";
-import {login, logout, register } from "../controllers/userController.js";
+import {login, logout, register, updatePassword } from "../controllers/userController.js";
+import isAuthenticated from "../middleware/isAuthenticated.js";
 // import isAuthenticated from "../middleware/isAuthenticated.js";
 
 const router = express.Router();
@@ -7,5 +8,6 @@ const router = express.Router();
 router.route("/register").post(register);
 router.route("/login").post(login);
 router.route("/logout").get(logout);
+router.route("/update-password").post(isAuthenticated, updatePassword);
 
 export default router;
