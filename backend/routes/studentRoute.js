29bc607a6b1caf2c564ba41cfc91c9e823ebb2f1
RohitShalgar4 <rohishalgar04@gmail.com>
2025-03-17 00:32:24 +0530

    changes in student route for update pass

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import { getStudentData } from '../controllers/studentController.js';
+import isAuthenticated from "../middleware/isAuthenticated.js";
+import checkRole from "../middleware/checkRole.js";
 
 const router = express.Router();
 
-// Route to fetch student data
-router.route('/').get(getStudentData);
-router.route('/:id').get(getStudentData);
+// All authenticated users can access student data
+router.route('/').get(isAuthenticated, getStudentData);
+
+// Specific student data can be accessed by admins, teachers, and the student themselves
+router.route('/:id').get(isAuthenticated, checkRole(["super_admin", "department_admin", "teacher", "student"]), getStudentData);
+
 export default router;
